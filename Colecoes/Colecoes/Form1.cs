85905e31284cc88b7b11246146c2b1a5d2c81f91
Luciano Henrique dos Santos <lh_santos@outlook.com>
2024-02-27 09:23:28 -0300

    Implementação de um array

--- Colecoes/Colecoes/Form1.cs
+++ Colecoes/Colecoes/Form1.cs
@@ -16,5 +16,30 @@ namespace Colecoes
         {
             InitializeComponent();
         }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            // Array
+            string[] nomes = new string[3];
+            nomes[0] = "Luciano";
+            nomes[1] = "Lilian";
+            nomes[2] = "Luana";
+
+            // Lista Generica
+            List<string> nomes2 = new List<string>();
+            nomes2.AddRange(nomes);
+            nomes2.Add("Lucas");
+            nomes2.Add("Princesa");
+
+            //lista.Items.Add(nomes2[0]);
+            //lista.Items.Add(nomes2[1]);
+            //lista.Items.Add(nomes2[2]);
+            //lista.Items.Add(nomes2[3]);
+            //lista.Items.Add(nomes2[4]);
+            foreach (string nome in nomes2)
+            {
+                lista.Items.Add(nome);
+            }
+        }
     }
 }
