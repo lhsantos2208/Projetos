87c0e2797b188cafc4a3f05c6e70b842e8f5f7e3
Luciano Henrique dos Santos <lh_santos@outlook.com>
2024-02-27 09:01:10 -0300

    Implementação dos metodos ToString, Convert.ToString e a classe Math.

--- ManipularNumeros/ManipularNumeros/Form1.cs
+++ ManipularNumeros/ManipularNumeros/Form1.cs
@@ -19,18 +19,63 @@ namespace ManipularNumeros
 
         private void btnExecutar_Click(object sender, EventArgs e)
         {
-            int num;
+            #region TryParse
+            //int num;
             //bool res =  int.TryParse(txtValor.Text, out num);
 
-            if (int.TryParse(txtValor.Text, out num))
-            {
-                lblResultado.Text = num.ToString();
-            }
-            else
-            {
-                lblResultado.Text = "Erro na conversão!";
-            }
-            
+            //if (int.TryParse(txtValor.Text, out num))
+            //{
+            //    lblResultado.Text = num.ToString();
+            //}
+            //else
+            //{
+            //    lblResultado.Text = "Erro na conversão!";
+            //}
+            #endregion
+
+            #region ToString/Convert.ToString
+            //double num = double.Parse(txtValor.Text);
+
+            //lblResultado.Text = num.ToString("00000.00");
+            //lblResultado.Text = num.ToString("#.00");
+
+            //int num = int.Parse(txtValor.Text);
+            //lblResultado.Text = num.ToString("X"); // Hex
+            //lblResultado.Text = num.ToString("X5"); // Hex definindo digitos
+            //lblResultado.Text = num.ToString("D"); // Dec
+            //lblResultado.Text = num.ToString("D5"); // Dec definindo digitos
+            //lblResultado.Text = num.ToString("C"); // Monetario
+
+            //lblResultado.Text = Convert.ToString(num, 16); // Hex
+            //lblResultado.Text = Convert.ToString(num, 10); // Dec
+            //lblResultado.Text = Convert.ToString(num, 8); // Octal
+
+            #endregion
+
+            #region Classe Math
+            //double pi = Math.PI;
+            //double E =  Math.E;
+            //double seno = Math.Sin(double.Parse(txtValor.Text));
+            //double conseno = Math.Cos(double.Parse(txtValor.Text));
+            //double potencia = Math.Pow(double.Parse(txtValor.Text), 3);
+            //double raizQuadrada = Math.Sqrt(double.Parse(txtValor.Text));
+            //double arredonda = Math.Round(double.Parse(txtValor.Text));
+            //double inteiro = Math.Truncate(double.Parse(txtValor.Text));
+            //double floor = Math.Floor(double.Parse(txtValor.Text));
+            //double ceil = Math.Ceiling(double.Parse(txtValor.Text));
+
+            //lblResultado.Text = pi.ToString("#.00");
+            //lblResultado.Text = E.ToString("#.00");
+            //lblResultado.Text = seno.ToString("#.00");
+            //lblResultado.Text = conseno.ToString("#.00");
+            //lblResultado.Text = potencia.ToString("#.00");
+            //lblResultado.Text = raizQuadrada.ToString("#.00");
+            //lblResultado.Text = arredonda.ToString("#.00");
+            //lblResultado.Text = inteiro.ToString("#.00");
+            //lblResultado.Text = floor.ToString("#.00");
+            //lblResultado.Text = ceil.ToString("#.00");
+
+            #endregion
         }
     }
 }
