e7248787cf4ee9b9ba0e013ec76b3e92ba5d9761
Luciano Henrique dos Santos <lh_santos@outlook.com>
2024-03-07 08:45:04 -0300

    Aplicação com MessageBox e Radom

---
+++ ClassesImportantes/ClassesImportantes/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ClassesImportantes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMessageBox = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btAleatorio = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMessageBox
+            // 
+            this.btnMessageBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMessageBox.Location = new System.Drawing.Point(12, 375);
+            this.btnMessageBox.Name = "btnMessageBox";
+            this.btnMessageBox.Size = new System.Drawing.Size(187, 63);
+            this.btnMessageBox.TabIndex = 0;
+            this.btnMessageBox.Text = "MessageBox";
+            this.btnMessageBox.UseVisualStyleBackColor = true;
+            this.btnMessageBox.Click += new System.EventHandler(this.btnMessageBox_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(13, 13);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(775, 97);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.Text = "label1";
+            // 
+            // btAleatorio
+            // 
+            this.btAleatorio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAleatorio.Location = new System.Drawing.Point(205, 375);
+            this.btAleatorio.Name = "btAleatorio";
+            this.btAleatorio.Size = new System.Drawing.Size(187, 63);
+            this.btAleatorio.TabIndex = 2;
+            this.btAleatorio.Text = "Aleatório";
+            this.btAleatorio.UseVisualStyleBackColor = true;
+            this.btAleatorio.Click += new System.EventHandler(this.btAleatorio_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btAleatorio);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnMessageBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMessageBox;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btAleatorio;
+    }
+}
+
---
+++ ClassesImportantes/ClassesImportantes/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassesImportantes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMessageBox_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Olá meus amigos !!");
+            //MessageBox.Show("Mensagem a ser impressa","Título da mensagem");
+            //DialogResult res = MessageBox.Show("Texto da mensagem", "Título da mensagem", MessageBoxButtons.YesNo);
+            //MessageBox.Show("Texto da mensagem", "Título da mensagem", MessageBoxButtons.YesNoCancel);
+            //DialogResult res = MessageBox.Show("Texto da mensagem", "Título da mensagem", MessageBoxButtons.OKCancel);
+
+            //if (res == DialogResult.OK) 
+            //{
+            //    lblResultado.Text = "O usuário clicou em OK!";
+            //}
+            //else if (res == DialogResult.Cancel)
+            //{
+            //    lblResultado.Text = "O usuário clicou em Cancelar!";
+            //}
+
+            //MessageBox.Show("Texto da mensagem", "Título da mensagem", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            MessageBox.Show("Texto da mensagem", "Título da mensagem", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information,MessageBoxDefaultButton.Button3);
+        }
+
+        private void btAleatorio_Click(object sender, EventArgs e)
+        {
+            Random r = new Random(DateTime.Now.Millisecond);
+
+            int valor = r.Next(100);
+            double valor2 = r.NextDouble() * 100;
+            //int valor = r.Next(1, 1000);
+
+            lblResultado.Text = "O número escolhido foi: " + valor2;
+        }
+    }
+}
