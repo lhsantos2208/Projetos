26272e910829d7a783099546493ed0fb6681310b
Luciano Henrique dos Santos <lh_santos@outlook.com>
2024-03-11 09:23:47 -0300

    Implementação com mais de uma forms parte 1

---
+++ Formulario/Formulario/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Formulario
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPrincipal = new System.Windows.Forms.Label();
+            this.btnSegunda = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPrincipal
+            // 
+            this.lblPrincipal.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrincipal.Location = new System.Drawing.Point(13, 13);
+            this.lblPrincipal.Name = "lblPrincipal";
+            this.lblPrincipal.Size = new System.Drawing.Size(775, 134);
+            this.lblPrincipal.TabIndex = 0;
+            this.lblPrincipal.Text = "Principal";
+            this.lblPrincipal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSegunda
+            // 
+            this.btnSegunda.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSegunda.Location = new System.Drawing.Point(19, 377);
+            this.btnSegunda.Name = "btnSegunda";
+            this.btnSegunda.Size = new System.Drawing.Size(201, 61);
+            this.btnSegunda.TabIndex = 1;
+            this.btnSegunda.Text = "Segunda Form";
+            this.btnSegunda.UseVisualStyleBackColor = true;
+            this.btnSegunda.Click += new System.EventHandler(this.btnSegunda_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSegunda);
+            this.Controls.Add(this.lblPrincipal);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Formulário Principal";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPrincipal;
+        private System.Windows.Forms.Button btnSegunda;
+    }
+}
+
---
+++ Formulario/Formulario/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSegunda_Click(object sender, EventArgs e)
+        {
+            Hide();
+            FormSegunda f = new FormSegunda();
+            //f.Show();
+            f.ShowDialog();
+            Show();
+        }
+    }
+}
