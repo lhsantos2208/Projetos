b1db3d70f0570ecf46e554dce8303bba5c68999a
Luciano Henrique dos Santos <lh_santos@outlook.com>
2024-03-11 11:08:51 -0300

    Implementação de controle Label via código fonte

---
+++ Formulario/FormControle/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormControle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Label label1 = new Label();
+            label1.Location = new Point(10,10);
+            label1.AutoSize = false;
+            label1.Size = new Size(500,100);
+            label1.BackColor = Color.White;
+            label1.ForeColor = Color.Black;
+            label1.Font = new Font(FontFamily.GenericSansSerif, 28, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Text = "Minha Label no Código.";
+            label1.Name = "label1";
+
+            this.Controls.Add(label1);
+        }
+    }
+}
