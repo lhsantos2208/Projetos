9733b2b68074549b03ac62770cd856dead0c4cac
Luciano Henrique dos Santos <lh_santos@outlook.com>
2024-03-12 11:50:37 -0300

    Implementação do projeto ControlsForm - ListView

---
+++ ControlesForms/Part010/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Part010
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lista = new System.Windows.Forms.ListView();
+            this.colNome = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colTelefone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colEmail = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lista
+            // 
+            this.lista.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colNome,
+            this.colTelefone,
+            this.colEmail});
+            this.lista.FullRowSelect = true;
+            this.lista.GridLines = true;
+            this.lista.HideSelection = false;
+            this.lista.LabelEdit = true;
+            this.lista.Location = new System.Drawing.Point(12, 12);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(776, 369);
+            this.lista.TabIndex = 0;
+            this.lista.UseCompatibleStateImageBehavior = false;
+            this.lista.View = System.Windows.Forms.View.Details;
+            this.lista.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lista_MouseDoubleClick);
+            // 
+            // colNome
+            // 
+            this.colNome.Text = "Nome";
+            this.colNome.Width = 200;
+            // 
+            // colTelefone
+            // 
+            this.colTelefone.Text = "Telefone";
+            this.colTelefone.Width = 200;
+            // 
+            // colEmail
+            // 
+            this.colEmail.Text = "Email";
+            this.colEmail.Width = 200;
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecutar.Location = new System.Drawing.Point(626, 387);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(162, 51);
+            this.btnExecutar.TabIndex = 1;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExecutar);
+            this.Controls.Add(this.lista);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lista;
+        private System.Windows.Forms.ColumnHeader colNome;
+        private System.Windows.Forms.ColumnHeader colTelefone;
+        private System.Windows.Forms.ColumnHeader colEmail;
+        private System.Windows.Forms.Button btnExecutar;
+    }
+}
+
---
+++ ControlesForms/Part010/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part010
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            ListViewItem item1 = new ListViewItem("Luciano");
+            item1.SubItems.Add("41 99206-8170");
+            item1.SubItems.Add("lhsantos@gmail.com");
+
+            ListViewItem item2 = new ListViewItem("Lilian");
+            item2.SubItems.Add("41 99191-4194");
+            item2.SubItems.Add("liliansantos_3@gmail.com");
+
+            ListViewItem item3 = new ListViewItem("Luana");
+            item3.SubItems.Add("41 99999-9999");
+            item3.SubItems.Add("luana.filha@gmail.com");
+
+            ListViewItem item4 = new ListViewItem(new string[] { "Lucas", "41 88888-8888", "lucas.filho@gmail.com" });
+
+            lista.Items.Clear();
+            lista.Items.Add(item1);
+            lista.Items.Add(item2);
+            lista.Items.Add(item3);
+            lista.Items.Add(item4);
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            //lista.Items.Clear();
+            //lista.Items.Remove(lista.SelectedItems[0]);
+            lista.Items.RemoveAt(1);
+        }
+
+        private void lista_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show(lista.SelectedItems[0].Text);
+        }
+    }
+}
