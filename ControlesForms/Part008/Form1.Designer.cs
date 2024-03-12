e56a82ea1d0e52b6ea63ff6f3f5867e03eb7ccbc
Luciano Henrique dos Santos <lh_santos@outlook.com>
2024-03-12 11:10:42 -0300

    Implementação do projeto ControlsForm - DateTimePicker

---
+++ ControlesForms/Part008/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Part008
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTime = new System.Windows.Forms.DateTimePicker();
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dateTime
+            // 
+            this.dateTime.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTime.Location = new System.Drawing.Point(39, 22);
+            this.dateTime.MaxDate = new System.DateTime(2025, 12, 31, 0, 0, 0, 0);
+            this.dateTime.MinDate = new System.DateTime(2010, 1, 1, 0, 0, 0, 0);
+            this.dateTime.Name = "dateTime";
+            this.dateTime.Size = new System.Drawing.Size(563, 38);
+            this.dateTime.TabIndex = 0;
+            this.dateTime.Value = new System.DateTime(2018, 1, 1, 0, 0, 0, 0);
+            this.dateTime.ValueChanged += new System.EventHandler(this.dateTime_ValueChanged);
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecutar.Location = new System.Drawing.Point(463, 333);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(325, 105);
+            this.btnExecutar.TabIndex = 1;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExecutar);
+            this.Controls.Add(this.dateTime);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTime;
+        private System.Windows.Forms.Button btnExecutar;
+    }
+}
+
---
+++ ControlesForms/Part008/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part008
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTime_ValueChanged(object sender, EventArgs e)
+        {
+           MessageBox.Show("Dia selecioando " + dateTime.Value.DayOfWeek);
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            DateTime atual = DateTime.Now;
+            dateTime.Value = atual;
+        }
+    }
+}
