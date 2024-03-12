8fd4aafc7df99e47e499cb33c17bad970c1c59ca
Luciano Henrique dos Santos <lh_santos@outlook.com>
2024-03-12 10:32:44 -0300

    Implementação do projeto ControlesForm - CheckListBox

---
+++ ControlesForms/Part006/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Part006
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkList = new System.Windows.Forms.CheckedListBox();
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkList
+            // 
+            this.checkList.CheckOnClick = true;
+            this.checkList.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkList.Dock = System.Windows.Forms.DockStyle.Left;
+            this.checkList.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkList.ForeColor = System.Drawing.Color.Maroon;
+            this.checkList.FormattingEnabled = true;
+            this.checkList.Location = new System.Drawing.Point(0, 0);
+            this.checkList.Name = "checkList";
+            this.checkList.Size = new System.Drawing.Size(400, 450);
+            this.checkList.Sorted = true;
+            this.checkList.TabIndex = 0;
+            this.checkList.SelectedIndexChanged += new System.EventHandler(this.checkList_SelectedIndexChanged);
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecutar.Location = new System.Drawing.Point(406, 12);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(382, 71);
+            this.btnExecutar.TabIndex = 1;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(407, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExecutar);
+            this.Controls.Add(this.checkList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkList;
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ControlesForms/Part006/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part006
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            checkList.Items.Clear();
+            checkList.Items.Add("Carro", true);
+            checkList.Items.Add("Moto", false);
+            checkList.Items.Add("Barco");
+            checkList.Items.Add("Avião");
+
+            int qtdItens = checkList.Items.Count;
+            //MessageBox.Show("Quantidade de itens " + qtdItens);
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            //checkList.Items.Clear();
+            //checkList.Items.AddRange(new string[] { "Carro", "Moto", "Avião", "Bicicleta" });
+
+            //bool res =  checkList.Items.Contains("Moto");
+            //if (res)
+            //{
+            //    MessageBox.Show("O item Moto esta na lista.");
+            //}
+
+            //checkList.Items.Remove("Moto");
+            //checkList.Items.RemoveAt(2);
+            //checkList.Items.Insert(1, "Cavalo");
+            //var itens = checkList.CheckedItems;
+            var itens = checkList.SelectedItems;
+
+            foreach (var item in itens)
+            {
+                MessageBox.Show(item.ToString());
+            }
+
+        }
+
+        private void checkList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label1.Text = checkList.SelectedItems.ToString();
+        }
+    }
+}
