791b253e06fa7115a126e9c9d0cf2febfb03c011
Luciano Henrique dos Santos <lh_santos@outlook.com>
2024-03-19 15:58:44 -0300

    Implementação do aplicativo Android com Abas

---
+++ Abas/Abas/Abas/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Abas
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProximo1_Clicked(object sender, EventArgs e)
+        {
+            CurrentPage = Children[1];
+        }
+
+        private void btnAnterior2_Clicked(object sender, EventArgs e)
+        {
+            CurrentPage = Children[0];
+        }
+
+        private void btnProximo2_Clicked(object sender, EventArgs e)
+        {
+            CurrentPage = Children[2];
+        }
+
+        private void btnAnterior3_Clicked(object sender, EventArgs e)
+        {
+            CurrentPage = Children[1];
+        }        
+    }
+}
